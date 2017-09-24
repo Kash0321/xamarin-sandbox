e19466da817b5c5876f5c3b307f2d640ad6e2499
Kash <sergio.castillo@pronet-ise.com>
2017-09-24 12:15:48 +0200

    Refactorización proyectos

---
+++ deep-linking/DeepLinking.Caller/DeepLinking.Caller/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DeepLinking.Caller
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            SubscribeToMessages();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            UnsubscribeFromMessages();
+        }
+
+        void SubscribeToMessages()
+        {
+            MessagingCenter.Subscribe<MainViewModel, string>(this, "GotoPage1", async (obj, p) =>
+            {
+                try
+                {
+                    await Navigation.PushAsync(new Page1View());
+                }
+                catch (Exception ex)
+                {
+                    MessagingCenter.Send<ContentPage, Exception>(this, "Error", ex);
+                }
+            });
+
+            MessagingCenter.Subscribe<MainViewModel, string>(this, "GotoPage2", async (obj, p) =>
+            {
+                try
+                {
+                    await Navigation.PushAsync(new Page2View());
+                }
+                catch (Exception ex)
+                {
+                    MessagingCenter.Send<ContentPage, Exception>(this, "Error", ex);
+                }
+            });
+
+
+            MessagingCenter.Subscribe<MainViewModel, Exception>(this, "Error", (obj, ex) =>
+            {
+                DisplayAlert("Shit", ex.ToString(), "Leave me alone");
+            });
+        }
+
+        void UnsubscribeFromMessages()
+        {
+            MessagingCenter.Unsubscribe<MainViewModel, string>(this, "GotoPage1");
+            MessagingCenter.Unsubscribe<MainViewModel, string>(this, "GotoPage2");
+        }
+    }
+}
