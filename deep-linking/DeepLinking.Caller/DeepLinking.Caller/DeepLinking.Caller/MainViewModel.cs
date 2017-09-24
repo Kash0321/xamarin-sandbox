a518859a846bf64e5a3b3ebc04708ffce8315645
Kash <sergio.castillo@pronet-ise.com>
2017-09-24 08:21:19 +0200

    Caller squeleton

---
+++ deep-linking/DeepLinking.Caller/DeepLinking.Caller/DeepLinking.Caller/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DeepLinking.Caller
+{
+    public class MainViewModel : ViewModel
+    {
+        public Command GotoPage1Command { get; set; }
+        public Command GotoPage2Command { get; set; }
+
+        public MainViewModel()
+        {
+            GotoPage1Command = new Command(() => GotoPage1());
+            GotoPage2Command = new Command(() => GotoPage2());
+        }
+
+        private void GotoPage1()
+        {
+            MessagingCenter.Send(this, "GotoPage1", "Page 1");
+        }
+
+        private void GotoPage2()
+        {
+            MessagingCenter.Send(this, "GotoPage2", "Page 1");
+        }
+    }
+}
---
+++ deep-linking/DeepLinking.Caller/DeepLinking.Caller/DeepLinking.Caller/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepLinking.Caller
+{
+
+    /// <summary>
+    /// Base de los ViewModels de toda la aplicación
+    /// </summary>
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Inicializa una instancia de <see cref="BaseViewModel"/>
+        /// </summary>
+        public ViewModel() { }
+
+        /// <summary>
+        /// Evento al que se suscriben las vistas medinte su mecanismo de bindeo, para actualizar la presentación
+        /// en función de los cambios que se producen en el ViewModel
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool isBusy;
+        /// <summary>
+        /// Flag para controlar si el ViewModel ya está ejecutado alguna otra operación
+        /// </summary>
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set
+            {
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Debe invocarse desde las clases derivadas, para procurar la notificación de cambios en los datos
+        /// del ViewModel hacia la Vista
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
