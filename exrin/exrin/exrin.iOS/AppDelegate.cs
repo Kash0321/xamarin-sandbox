987a059a7b2f3a3a600dbe9f563e360dac7166ef
Kash <sergio.castillo@pronet-ise.com>
2017-10-13 15:47:22 +0200

    [POF Exrin] - Inicio

---
+++ exrin/exrin/exrin.iOS/AppDelegate.cs
@@ -0,0 +1,19 @@
+﻿
+using Foundation;
+using UIKit;
+
+namespace exrin.iOS
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init();
+            Exrin.Framework.App.Init();
+            LoadApplication(new App());
+
+			return base.FinishedLaunching(app, options);
+		}
+	}
+}
---
+++ exrin/exrin/exrin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using exrin.ViewModels;
+
+using Xamarin.Forms;
+
+namespace exrin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
